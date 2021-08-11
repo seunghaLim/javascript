8ffd72688fe1a9022b553db7169f5d827f6ec17e
seungha <tmdgk490255@gmail.com>
2021-08-12 02:47:55 +0900

    0811_js_study

---
+++ comunity_MGC/day_3/index.js
@@ -0,0 +1,14 @@
+var h3 = document.getElementsByTagName('h3');
+
+function changeText1(){
+    h3[0].innerHTML = "모처럼이니"
+}
+
+function changeText2(){
+    h3[1].innerHTML = "각잡고 열심히하면"
+}
+
+function changeText3(){
+    h3[2].innerHTML = "코린이 탈출이다"
+}
+
