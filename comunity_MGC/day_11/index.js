7e1247d3060bfceee4879e621f9d26cba846db7c
seungha <tmdgk490255@gmail.com>
2021-08-24 21:13:57 +0900

    0823_js_study

---
+++ comunity_MGC/day_11/index.js
@@ -0,0 +1,10 @@
+function check_text(){
+    var v = document.getElementById("console").value;
+
+    if (v == "안아줘"){
+        console.log("꺼져");
+    }
+    else{
+        console.log(v);
+    }
+}
